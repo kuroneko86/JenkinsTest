348cd37864b273cb012210f9a012eff161d24902
Michael Miller <mmiller@kuronekokukan.com>
2020-08-13 16:33:45 -0400

    add results checker

--- vars/getSonarResults.groovy
+++ vars/getSonarResults.groovy
@@ -1,3 +1,24 @@
 def call() {
-    def execSonar = steps.sh(script: "curl http://192.168.10.106:9000/api/ce/task?id=AXPpWR2kr3-nW6vSSpXd")
+    def loopLimit = 12
+    def loopinterator = 0
+    def checkStatusPattern = '"status":"SUCCESS"'
+    def continue = false
+    
+    do {
+        printlin "Checking results..."
+        def getSonarResults = steps.sh(script: "curl $sonarTaskID", returnStdout: true)
+        if(getSonarResults =~ checkStatusPattern) {
+            printlin "Results completed"
+            $continue = true
+        }
+        elseif (loopinterator >= loopLimit{
+            printlin "Results are still not ready, aborting"
+            $continue = true
+        }
+        else {
+            printlin "Results not ready yet, waiting 5 seconds";
+            loopinterator++
+            sleep(5000)
+        }
+    } while ($continue)
 }
