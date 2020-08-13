20e9e0d4d6a2c73184bb0c33e3f70658d84a3877
Michael Miller <mmiller@kuronekokukan.com>
2020-08-13 16:12:32 -0400

    new process handling

--- vars/deployDocker.groovy
+++ vars/deployDocker.groovy
@@ -2,7 +2,7 @@ def call() {
     def sout = new StringBuilder(), serr = new StringBuilder()
     def proc = 'docker ps -f name=${containerName}'.execute()
     proc.consumeProcessOutput(sout, serr)
-    proc.waitForOrKill(1000)
+    proc.waitForOrKill(30000)
     println "out> $sout err> $serr"
     if($sout.indexOf(${containerName}))
     {
--- vars/runSonarScanner.groovy
+++ vars/runSonarScanner.groovy
@@ -1,7 +1,11 @@
 def call() {
     def taskIDPattern = "INFO: More about the report processing at (.*)"
-    def execSonar = steps.sh(script: "sonar-scanner-4.4.0.2170-linux/bin/sonar-scanner -Dsonar.projectKey=${sonarProjectKey} -Dsonar.sources=${sonarFolder} -Dsonar.host.url=http://192.168.10.106:9000 -Dsonar.login=${sonarLogin}")
-    def sonarTaskID = (execSonar =~ taskIDPattern).findAll()
-    println execSonar
+    //def execSonar = steps.sh(script: "sonar-scanner-4.4.0.2170-linux/bin/sonar-scanner -Dsonar.projectKey=${sonarProjectKey} -Dsonar.sources=${sonarFolder} -Dsonar.host.url=http://192.168.10.106:9000 -Dsonar.login=${sonarLogin}")
+    def sout = new StringBuilder(), serr = new StringBuilder()
+    def proc = "sonar-scanner-4.4.0.2170-linux/bin/sonar-scanner -Dsonar.projectKey=$sonarProjectKey -Dsonar.sources=$sonarFolder -Dsonar.host.url=http://192.168.10.106:9000 -Dsonar.login=$sonarLogin".execute()
+    proc.consumeProcessOutput(sout, serr)
+    proc.waitForOrKill(30000)
+    println "out> $sout err> $serr"
+    def sonarTaskID = (sout =~ taskIDPattern).findAll()
     println sonarTaskID.first();
 }
