7fdfabfddf1284f3f004051ba76debc08f442ce9
Michael Miller <mmiller@kuronekokukan.com>
2020-08-13 14:59:22 -0400

    less sudo, more scanning

--- vars/installSonarScanner.groovy
+++ vars/installSonarScanner.groovy
@@ -1,5 +1,5 @@
 def call() {
     sh "curl https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.4.0.2170-linux.zip --output sonarscanner.zip"
-    sh "sudo apt install unzip"
+    sh "apt install unzip"
     sh "unzip sonarscanner.zip -d sonarscanner"
 }
---
+++ vars/runSonarScanner.groovy
@@ -0,0 +1,3 @@
+def call() {
+    echo "sonar-scanner-4.4.0.2170-linux\bin\sonar-scanner -Dsonar.projectKey=Codenames -Dsonar.sources=. -Dsonar.host.url=http://192.168.10.106:9000 -Dsonar.login=21a8f9175e9baeda554b77f2d52c60533229aebd"
+}
