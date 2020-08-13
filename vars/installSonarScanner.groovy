811dafb51deb5b65e49fac18ecfbd458b40ea437
Michael Miller <mmiller@kuronekokukan.com>
2020-08-13 14:55:20 -0400

    fucking quotes

--- vars/installSonarScanner.groovy
+++ vars/installSonarScanner.groovy
@@ -1,5 +1,5 @@
 def call() {
     sh "curl https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.4.0.2170-linux.zip --output sonarscanner.zip"
     sudo apt install unzip
-    unzip sonarscanner.zip -d sonarscanner
+    sh "unzip sonarscanner.zip -d sonarscanner"
 }
