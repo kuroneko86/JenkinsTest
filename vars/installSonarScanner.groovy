597672b118ee698cfe9a9923dae6d281835803c2
Michael Miller <mmiller@kuronekokukan.com>
2020-08-13 14:53:11 -0400

    Corrected file name

--- vars/installSonarScanner.groovy
+++ vars/installSonarScanner.groovy
@@ -1,5 +1,5 @@
 def call() {
     sh "curl https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.4.0.2170-linux.zip --output sonarscanner.zip"
     sudo apt install unzip
-    unzip sonarscanner -d sonarscanner
+    unzip sonarscanner.zip -d sonarscanner
 }
