54a118618fce04688071476f2bd1d9cc8e4445a9
Michael Miller <mmiller@kuronekokukan.com>
2020-08-13 16:18:04 -0400

    path fix

--- vars/runSonarScanner.groovy
+++ vars/runSonarScanner.groovy
@@ -3,8 +3,7 @@ def call() {
     def taskIDPattern = "INFO: More about the report processing at (.*)"
     //def execSonar = steps.sh(script: "sonar-scanner-4.4.0.2170-linux/bin/sonar-scanner -Dsonar.projectKey=${sonarProjectKey} -Dsonar.sources=${sonarFolder} -Dsonar.host.url=http://192.168.10.106:9000 -Dsonar.login=${sonarLogin}")
     def sout = new StringBuilder(), serr = new StringBuilder()
-    //def proc = "sonar-scanner-4.4.0.2170-linux/bin/sonar-scanner -Dsonar.projectKey=$sonarProjectKey -Dsonar.sources=$sonarFolder -Dsonar.host.url=http://192.168.10.106:9000 -Dsonar.login=$sonarLogin".execute()
-    def proc = "$workspace/ls -al".execute()
+    def proc = "$workspace/sonar-scanner-4.4.0.2170-linux/bin/sonar-scanner -Dsonar.projectKey=$sonarProjectKey -Dsonar.sources=$sonarFolder -Dsonar.host.url=http://192.168.10.106:9000 -Dsonar.login=$sonarLogin".execute()
     proc.consumeProcessOutput(sout, serr)
     proc.waitForOrKill(30000)
     println "out> $sout err> $serr"
