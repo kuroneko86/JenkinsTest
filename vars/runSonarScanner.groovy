d6c6b6aa1238a0fceec67281e50b48101660bafa
Michael Miller <mmiller@kuronekokukan.com>
2020-08-13 16:14:57 -0400

    debug

--- vars/runSonarScanner.groovy
+++ vars/runSonarScanner.groovy
@@ -2,7 +2,8 @@ def call() {
     def taskIDPattern = "INFO: More about the report processing at (.*)"
     //def execSonar = steps.sh(script: "sonar-scanner-4.4.0.2170-linux/bin/sonar-scanner -Dsonar.projectKey=${sonarProjectKey} -Dsonar.sources=${sonarFolder} -Dsonar.host.url=http://192.168.10.106:9000 -Dsonar.login=${sonarLogin}")
     def sout = new StringBuilder(), serr = new StringBuilder()
-    def proc = "sonar-scanner-4.4.0.2170-linux/bin/sonar-scanner -Dsonar.projectKey=$sonarProjectKey -Dsonar.sources=$sonarFolder -Dsonar.host.url=http://192.168.10.106:9000 -Dsonar.login=$sonarLogin".execute()
+    //def proc = "sonar-scanner-4.4.0.2170-linux/bin/sonar-scanner -Dsonar.projectKey=$sonarProjectKey -Dsonar.sources=$sonarFolder -Dsonar.host.url=http://192.168.10.106:9000 -Dsonar.login=$sonarLogin".execute()
+    def proc = "ls -al".execute()
     proc.consumeProcessOutput(sout, serr)
     proc.waitForOrKill(30000)
     println "out> $sout err> $serr"
