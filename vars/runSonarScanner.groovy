b34234d85fb1a8ecace418c115204e987b8f0aa4
Michael Miller <mmiller@kuronekokukan.com>
2020-08-13 16:04:00 -0400

    parens!

--- vars/runSonarScanner.groovy
+++ vars/runSonarScanner.groovy
@@ -1,6 +1,6 @@
 def call() {
     def taskIDPattern = "INFO: More about the report processing at (.*)"
-    def execSonar = steps.sh(script: "sonar-scanner-4.4.0.2170-linux/bin/sonar-scanner -Dsonar.projectKey=${sonarProjectKey} -Dsonar.sources=${sonarFolder} -Dsonar.host.url=http://192.168.10.106:9000 -Dsonar.login=${sonarLogin}"
+    def execSonar = steps.sh(script: "sonar-scanner-4.4.0.2170-linux/bin/sonar-scanner -Dsonar.projectKey=${sonarProjectKey} -Dsonar.sources=${sonarFolder} -Dsonar.host.url=http://192.168.10.106:9000 -Dsonar.login=${sonarLogin}")
     def sonarTaskID = (execSonar =~ taskIDPattern)
     println sonarTaskID  
 }
