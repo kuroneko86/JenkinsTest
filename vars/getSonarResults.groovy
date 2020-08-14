8a1651aecc80bff835105284fe19d9f0212bd68b
Michael Miller <mmiller@kuronekokukan.com>
2020-08-13 20:40:31 -0400

    strings

--- vars/getSonarResults.groovy
+++ vars/getSonarResults.groovy
@@ -4,7 +4,7 @@ def call() {
     def checkStatusPattern = '"status":"SUCCESS"'
     def proceed = 0
 
-    while ($proceed) {
+    while (proceed) {
         printlin "Checking report readiness..."
         def getSonarResults = steps.sh(script: "curl $sonarTaskID", returnStdout: true)
         if(getSonarResults =~ checkStatusPattern) {
@@ -21,22 +21,27 @@ def call() {
         }
     }
 
-    if(!$abort) {
+    if(!abort) {
         def resultsPattern = 'total":([^,]*)'
         def severityLimit = 25
 
         def checkBlockers = steps.sh(script: "curl http://192.168.10.106:9000/api/issues/search?pageSize=500&componentKeys=$sonarProjectKey&ps=500&p=1&severities=BLOCKER", returnStdout: true)
         def resultsBlockers = (checkBlockers =~ taskIDPattern).findAll().first()
-        if($resultsBlockers > 0)
-        {
+        if(resultsBlockers > 0) {
             error("Blocker found in results, aborting")
         }
+        else {
+            println "No blockers found"
+        }
+
 
         def checkSeverity = steps.sh(script: "curl http://192.168.10.106:9000/api/issues/search?pageSize=500&componentKeys=$sonarProjectKey&ps=500&p=1&severities=CRITICAL,MAJOR", returnStdout: true)
         def resultsSeverity = (checkSeverity =~ taskIDPattern).findAll().first()
-        if($resultsSeverity > $severityLimit)
-        {
+        if(resultsSeverity > severityLimit) {
             error("Too many errors reported, aborting")
+        }
+        else {
+            println "Number of errors (" + resultsSeverity + "") below limit (" + severityLimit + ") found"
         }
     }
 }
