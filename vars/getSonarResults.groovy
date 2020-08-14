6784a5a1cac0a25dc836e6d78e87477eaf0ab720
Michael Miller <mmiller@kuronekokukan.com>
2020-08-13 20:36:39 -0400

    parens!

--- vars/getSonarResults.groovy
+++ vars/getSonarResults.groovy
@@ -11,7 +11,7 @@ def call() {
             printlin "Report ready"
             $proceed = 1
         }
-        else if (loopinterator >= loopLimit{
+        else if (loopinterator >= loopLimit) {
             error ("Report is taking too long, aborting")            
         }
         else {
