27f2593222232cc9217839bec13bb6afacc97825
Michael Miller <mmiller@kuronekokukan.com>
2020-08-14 10:08:23 -0400

    shimmy

--- src/com/kukan/sonarQube.groovy
+++ src/com/kukan/sonarQube.groovy
@@ -2,10 +2,11 @@ package com.kukan
 
 import groovy.json.JsonSlurperClassic
 
-@NONCPS def getSonarQubeResults(String url)
+@NONCPS
+def getSonarQubeResults(String url)
 {
     println url
-    
+
     def jsonSlurper = new JsonSlurperClassic()
     def getSonarResults = steps.sh(script: "curl " + url, returnStdout: true)
     def reportStatus = jsonSlurper.parseText(getSonarResults)
