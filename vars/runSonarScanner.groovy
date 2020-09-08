b9819310c810a539811f1303904c361e3121799d
Michael Miller <mmiller@kuronekokukan.com>
2020-09-08 18:55:45 -0400

    capitals

--- vars/runSonarScanner.groovy
+++ vars/runSonarScanner.groovy
@@ -1,4 +1,4 @@
-def call(string sonarProjectKey, string sonarLogin) {
+def call(String sonarProjectKey, String sonarLogin) {
     def workspace = WORKSPACE
     def taskIDPattern = "INFO: More about the report processing at (.*)"
     def execSonar = steps.sh(script: "sonar-scanner-4.4.0.2170-linux/bin/sonar-scanner -Dsonar.projectKey=${sonarProjectKey} -Dsonar.sources=. -Dsonar.host.url=http://192.168.10.106:9000 -Dsonar.login=${sonarLogin}", returnStdout: true)
