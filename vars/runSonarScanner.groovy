11397e7946b2f87d069249d7cb3fb501ed9f652a
Michael Miller <mmiller@kuronekokukan.com>
2020-09-08 18:52:36 -0400

    adjust

--- vars/runSonarScanner.groovy
+++ vars/runSonarScanner.groovy
@@ -1,7 +1,7 @@
-def call(string sonarProjectKey, string sonarLogin, string sonarFolder = ".") {
+def call(string sonarProjectKey, string sonarLogin) {
     def workspace = WORKSPACE
     def taskIDPattern = "INFO: More about the report processing at (.*)"
-    def execSonar = steps.sh(script: "sonar-scanner-4.4.0.2170-linux/bin/sonar-scanner -Dsonar.projectKey=${sonarProjectKey} -Dsonar.sources=${sonarFolder} -Dsonar.host.url=http://192.168.10.106:9000 -Dsonar.login=${sonarLogin}", returnStdout: true)
+    def execSonar = steps.sh(script: "sonar-scanner-4.4.0.2170-linux/bin/sonar-scanner -Dsonar.projectKey=${sonarProjectKey} -Dsonar.sources=. -Dsonar.host.url=http://192.168.10.106:9000 -Dsonar.login=${sonarLogin}", returnStdout: true)
     println execSonar
     def sonarTaskID = (execSonar =~ taskIDPattern).findAll().first()
     println sonarTaskID
