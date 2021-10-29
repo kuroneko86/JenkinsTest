38e666691240e9bb5a690a4e25c95a8ec8e7cf16
kuroneko86 <46491378+kuroneko86@users.noreply.github.com>
2021-10-29 09:53:01 -0400

    Update sonar binaries

--- vars/installSonarScanner.groovy
+++ vars/installSonarScanner.groovy
@@ -1,5 +1,5 @@
 def call() {
-    sh "curl https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.4.0.2170-linux.zip --output sonarscanner.zip"
+    sh "curl https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.6.2.2472-linux.zip --output sonarscanner.zip"
     //sh "apt install unzip"
     sh "unzip sonarscanner.zip"
 }
