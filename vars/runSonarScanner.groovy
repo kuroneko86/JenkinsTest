145589069545b4ad8a3637dba670157cd5c81944
kuroneko86 <46491378+kuroneko86@users.noreply.github.com>
2021-10-29 10:12:44 -0400

    Update sonar path

--- vars/runSonarScanner.groovy
+++ vars/runSonarScanner.groovy
@@ -1,7 +1,7 @@
 def call(String sonarProjectKey, String sonarLogin) {
     def workspace = WORKSPACE
     def taskIDPattern = "INFO: More about the report processing at (.*)"
-    def execSonar = steps.sh(script: "sonar-scanner-4.4.0.2170-linux/bin/sonar-scanner -Dsonar.projectKey=${sonarProjectKey} -Dsonar.sources=. -Dsonar.host.url=http://192.168.10.106:9000 -Dsonar.login=${sonarLogin}", returnStdout: true)
+    def execSonar = steps.sh(script: "sonar-scanner-4.6.2.2472-linux/bin/sonar-scanner -Dsonar.projectKey=${sonarProjectKey} -Dsonar.sources=. -Dsonar.host.url=http://192.168.10.106:9000 -Dsonar.login=${sonarLogin}", returnStdout: true)
     println execSonar
     def sonarTaskID = (execSonar =~ taskIDPattern).findAll().first()
     println sonarTaskID
