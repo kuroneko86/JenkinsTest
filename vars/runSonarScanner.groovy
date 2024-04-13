a23a7801b24e620c92d57e1791dbf2cd90e14eb1
Michael Miller <michaeljaymiller@gmail.com>
2024-04-12 20:30:40 -0400

    Add debug flag for sonar

--- vars/runSonarScanner.groovy
+++ vars/runSonarScanner.groovy
@@ -1,7 +1,7 @@
 def call(String sonarProjectKey, String sonarLogin) {
     def workspace = WORKSPACE
     def taskIDPattern = "INFO: More about the report processing at (.*)"
-    def execSonar = steps.sh(script: "sonar-scanner-5.0.1.3006-linux/bin/sonar-scanner -Dsonar.projectKey=${sonarProjectKey} -Dsonar.sources=. -Dsonar.host.url=http://192.168.10.37:9000 -Dsonar.login=${sonarLogin}", returnStdout: true)
+    def execSonar = steps.sh(script: "sonar-scanner-5.0.1.3006-linux/bin/sonar-scanner -X -Dsonar.projectKey=${sonarProjectKey} -Dsonar.sources=. -Dsonar.host.url=http://192.168.10.37:9000 -Dsonar.login=${sonarLogin}", returnStdout: true)
     println execSonar
     def sonarTaskID = (execSonar =~ taskIDPattern).findAll().first()
     println sonarTaskID
