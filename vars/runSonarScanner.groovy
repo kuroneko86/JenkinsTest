896c35e5c90dc39f22b79e62f04fc48b14240833
Michael Miller <michaeljaymiller@gmail.com>
2024-11-13 16:58:11 -0500

    Move sonar settings to project Upgrade sonar scanner

--- vars/runSonarScanner.groovy
+++ vars/runSonarScanner.groovy
@@ -1,10 +1,10 @@
 def call(String sonarProjectKey, String sonarLogin) {
     def workspace = WORKSPACE
     def taskIDPattern = "INFO: More about the report processing at (.*)"
-    def execSonar = steps.sh(script: "sonar-scanner-4.8.1.3023-linux/bin/sonar-scanner -Dsonar.projectKey=${sonarProjectKey} -Dsonar.sources=. -Dsonar.exclusions=tests/** -Dsonar.php.coverage.reportPaths=tests/results/clover.xml -Dsonar.host.url=http://192.168.10.37:9000 -Dsonar.login=${sonarLogin}", returnStdout: true)
+    def execSonar = steps.sh(script: "sonar-scanner-6.2.1.4610-linux-x64/bin/sonar-scanner -Dsonar.host.url=http://192.168.10.37:9000", returnStdout: true)
     println execSonar
     def sonarTaskID = (execSonar =~ taskIDPattern).findAll().first()
     println sonarTaskID
     env.sonarTaskID = sonarTaskID[1]
-    def removeSonar = steps.sh(script: "rm sonar-scanner-4.8.1.3023-linux/ -r")
+    def removeSonar = steps.sh(script: "rm sonar-scanner-6.2.1.4610-linux-x64/ -r")
 }
