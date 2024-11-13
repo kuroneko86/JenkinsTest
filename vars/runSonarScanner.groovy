d16edb5094dcb0f05d6aa009243cacca7acd0a86
kuroneko86 <46491378+kuroneko86@users.noreply.github.com>
2024-11-13 11:28:52 -0500

    Update runSonarScanner.groovy
    Add php code coverage path

--- vars/runSonarScanner.groovy
+++ vars/runSonarScanner.groovy
@@ -1,7 +1,7 @@
 def call(String sonarProjectKey, String sonarLogin) {
     def workspace = WORKSPACE
     def taskIDPattern = "INFO: More about the report processing at (.*)"
-    def execSonar = steps.sh(script: "sonar-scanner-4.8.1.3023-linux/bin/sonar-scanner -Dsonar.projectKey=${sonarProjectKey} -Dsonar.sources=. -Dsonar.host.url=http://192.168.10.37:9000 -Dsonar.login=${sonarLogin}", returnStdout: true)
+    def execSonar = steps.sh(script: "sonar-scanner-4.8.1.3023-linux/bin/sonar-scanner -Dsonar.projectKey=${sonarProjectKey} -Dsonar.sources=. -Dsonar.php.coverage.reportPaths=tests/xml-coverage/ -Dsonar.host.url=http://192.168.10.37:9000 -Dsonar.login=${sonarLogin}", returnStdout: true)
     println execSonar
     def sonarTaskID = (execSonar =~ taskIDPattern).findAll().first()
     println sonarTaskID
