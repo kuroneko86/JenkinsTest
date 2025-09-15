e305e43e9e526b971a83ab438015949b318d78fd
Michael Miller <michjaeljaymiller@gmail.com>
2025-09-15 16:07:19 -0400

    Fix variable name in cleanup step

--- vars/cleanupSonarScanner.groovy
+++ vars/cleanupSonarScanner.groovy
@@ -1,4 +1,4 @@
 def call() {
-    def removeSonar2 = steps.sh(script: "rm .scannerwork -rf")
+    def removeSonar = steps.sh(script: "rm .scannerwork -rf")
     def removeSonar2 = steps.sh(script: "rm sonar* -rf")
 } 
