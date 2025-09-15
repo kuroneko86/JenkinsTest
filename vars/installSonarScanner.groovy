2e37168e5b5477fd0d1e3ae8c0b81be4f8fb5155
Michael Miller <michjaeljaymiller@gmail.com>
2025-09-15 15:54:58 -0400

    Add delete to the install step

--- vars/installSonarScanner.groovy
+++ vars/installSonarScanner.groovy
@@ -1,5 +1,5 @@
 def call() {
+    sh "rm -rf sonarscanner.zip"
     sh "curl https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-6.2.1.4610-linux-x64.zip --output sonarscanner.zip"
-    //sh "apt install unzip"
     sh "unzip -q sonarscanner.zip"
 }
